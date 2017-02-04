e8cb9bafa12b8ce8ebbb4e24cd7ead72451410c3
Alex Volkov <vol4ikman@gmail.com>
2017-02-04 23:29:54 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,21 +3,21 @@
 **  Define
 *****************************************/
 if( !defined('THEME') ){
-	define("THEME", get_template_directory_uri());
+define("THEME", get_template_directory_uri());
 } 
 //if wpml
 // define("LANG",ICL_LANGUAGE_CODE);
 define("LANG", "he");
 if(is_rtl()){
-	define("FLOAT", 'right');
-	define("FOUNDATION", THEME.'/assets/foundation-6.2.1-rtl');
+define("FLOAT", 'right');
+define("FOUNDATION", THEME.'/assets/foundation-6.2.1-rtl');
 }
 else{
-	define("FLOAT", 'left');
-	define("FOUNDATION", THEME.'/assets/foundation-6.2.1-ltr');
+define("FLOAT", 'left');
+define("FOUNDATION", THEME.'/assets/foundation-6.2.1-ltr');
 }
 if( !defined('TEMPLATEPATH') ) {
-	define( 'TEMPLATEPATH', get_template_directory() );    
+define( 'TEMPLATEPATH', get_template_directory() );    
 }   	
 /*****************************************
 **  Languages
