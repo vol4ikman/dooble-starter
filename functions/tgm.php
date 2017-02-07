9fe677a9b7d9b4392e5c87d660c5afdcc8588ecc
Alex Volkov <vol4ikman@gmail.com>
2017-02-07 11:03:48 +0200

    Update tgm.php

--- functions/tgm.php
+++ functions/tgm.php
@@ -40,16 +40,16 @@ function gulp_starter_register_required_plugins() {
 	$plugins = array(
 
 		array(
-			'name'               => 'TGM Example Plugin', // The plugin name.
-			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
-			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+			'name'               => 'Advanced custom fields Pro', // The plugin name.
+			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/functions/tgm/plugins/advanced-custom-fields-pro.zip', // The plugin source.
+			'required'           => true,
 		),
+		array(
+			'name'	   => 'Contact Form 7',
+			'slug'	   => 'contact-form-7',
+			'required' => true,
+		),		
 		array(
 			'name'	   => 'Regenerate Thumbnails',
 			'slug'	   => 'regenerate-thumbnails',
@@ -64,11 +64,6 @@ function gulp_starter_register_required_plugins() {
 			'name'	   => 'Duplicate Post',
 			'slug'	   => 'duplicate-post',
 			'required' => false,
-		),					
-		array(
-			'name'	   => 'Contact Form 7',
-			'slug'	   => 'contact-form-7',
-			'required' => true,
 		)
 
 	);
