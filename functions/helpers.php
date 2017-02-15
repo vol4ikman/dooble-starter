a2037fb69ba884eac3bb260a722b80b480d19cc8
Alex Volkov <vol4ikman@gmail.com>
2017-02-15 08:57:53 +0200

    Update helpers.php

--- functions/helpers.php
+++ functions/helpers.php
@@ -2,3 +2,13 @@
 /*********************
 ** Helper functions
 **********************/
+
+//ACF Google API
+function get_google_api_key(){
+    $google_api_key = get_field('google_api_key','option');
+    return $google_api_key;
+}
+function google_api_acf_init() {
+	acf_update_setting('google_api_key', get_google_api_key());
+}
+add_action('acf/init', 'google_api_acf_init');
