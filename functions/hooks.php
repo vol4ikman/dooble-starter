1ed321a59cb823c746a28372334a3eede1e15761
Alex Volkov <vol4ikman@gmail.com>
2017-02-15 08:45:19 +0200

    Update hooks.php

--- functions/hooks.php
+++ functions/hooks.php
@@ -1,31 +1,4 @@
 <?php
-define('CSS_INC', THEME.'/build/css/');
-define('JS_INC', THEME.'/build/js/');
-define('ENV', 'dev');
-
-// Load styles
-function qs_theme_styles(){
-    if(ENV == 'dev'){
-        wp_register_style('assets', CSS_INC . 'assets.min.css', array(), NULL, 'all'); wp_enqueue_style('assets');
-        wp_register_style('main-style', CSS_INC . 'main-style.css', array(), NULL, 'all'); wp_enqueue_style('main-style');
-        wp_register_style('responsive', CSS_INC . 'responsive.css', array(), NULL, 'all'); wp_enqueue_style('responsive');
-    } else {
-        wp_register_style('production', CSS_INC . 'production.min.css', array(), NULL, 'all'); wp_enqueue_style('production');
-    }
-}
-add_action('wp_enqueue_scripts', 'qs_theme_styles');
-
-// Load scripts
-function qs_theme_scripts() {
-    if(ENV == 'dev'){
-        wp_register_script( 'assets', JS_INC . 'assets.min.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'assets' );
-        wp_register_script( 'scripts', JS_INC . 'scripts.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'scripts' );
-    } else {
-        wp_register_script( 'production', JS_INC . 'production.min.js', array( 'jquery' ), NULL, true ); wp_enqueue_script( 'production' );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'qs_theme_scripts' );
-
 if ( ! function_exists( 'add_body_class' ) ){
     function add_body_class( $classes ) {
         global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
