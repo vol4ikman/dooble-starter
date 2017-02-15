f206835281876900a7e49387db262cd09e9cd741
Alex Volkov <vol4ikman@gmail.com>
2017-02-15 15:13:17 +0200

    Update options.php

--- admin/options.php
+++ admin/options.php
@@ -2,12 +2,14 @@
 if( function_exists('acf_add_options_page') ) {
 
     acf_add_options_page(array(
-        'page_title' 	=> 'General Settings',
-        'menu_title'	=> 'Settings',
+        'page_title' 	=> 'Site Settings',
+        'menu_title'	=> 'Site Settings',
         'menu_slug' 	=> 'theme-general-settings',
         'capability'	=> 'edit_posts',
         'redirect'		=> false
     ));
+
+    include('acf-options-import.php');
 
     // acf_add_options_sub_page(array(
     //     'page_title' 	=> 'Header Settings',
