e891dab0b37b35ea99debad8751467699ff1f103
Alex Volkov <vol4ikman@gmail.com>
2017-02-20 12:34:36 +0200

    update favicons

--- header.php
+++ header.php
@@ -8,6 +8,26 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="format-detection" content="telephone=no">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<?php /* Please create favicon files with http://iconogen.com/ */ ?>
+	<link rel="shortcut icon" href="<?php echo $imagesFolder; ?>favicon/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo THEME; ?>/images/favicon/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="<?php echo THEME; ?>/images/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="<?php echo THEME; ?>/images/favicon/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo THEME; ?>/images/favicon/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo THEME; ?>/images/favicon/android-chrome-192x192.png" sizes="192x192">
+	<meta name="msapplication-square70x70logo" content="<?php echo THEME; ?>/images/favicon/smalltile.png" />
+	<meta name="msapplication-square150x150logo" content="<?php echo THEME; ?>/images/favicon/mediumtile.png" />
+	<meta name="msapplication-wide310x150logo" content="<?php echo THEME; ?>/images/favicon/widetile.png" />
+	<meta name="msapplication-square310x310logo" content="<?php echo THEME; ?>/images/favicon/largetile.png" />
+	
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
