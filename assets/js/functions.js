d4c9e5c7214862d621770735a624afc0674e2b05
Alex Volkov <vol4ikman@gmail.com>
2017-03-20 12:26:45 +0200

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -30,3 +30,18 @@ function animate_scroll(target){
         scrollTop: jQuery(target).offset().top
     }, 1000);
 }
+
+/**************************
+    Accessibility
+**************************/   
+jQuery('.wrap_main_menu nav ul li.menu-item-has-children a').on('focusin',function(e){
+    e.preventDefault();
+    var this_el = jQuery(this);
+    this_el.parent().addClass('hover');
+});
+
+jQuery('body').on('focusout','ul li.menu-item-has-children.hover ul li:last-child a', function(e){
+    e.preventDefault();
+    var this_el = jQuery(this);
+    this_el.parent().parent().parent().removeClass('hover');
+});
