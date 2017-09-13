4b621b54b71779caaa3507cb2d3929bfbc3bd898
Alex Volkov <vol4ikman@gmail.com>
2017-09-13 11:43:48 +0300

    update file name from front-ajax to ajax

--- functions/ajax.php
+++ functions/ajax.php
@@ -1,9 +1,4 @@
 <?php
-/**
-* add_frontend_ajax_javascript_file()
-* Enqueue Ajax Scripts
-* @return
-*/
 add_action( 'wp_enqueue_scripts', 'add_frontend_ajax_javascript_file' );
 function add_frontend_ajax_javascript_file() {
     wp_enqueue_script( 'ajax_custom_script', THEME . '/assets/js/ajax.js', array('jquery') );
