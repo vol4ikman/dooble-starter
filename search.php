36a10e00c0a52d4767cb44a2da293c641504459b
Alex Volkov <vol4ikman@gmail.com>
2017-10-11 11:24:26 +0300

    Update search.php

--- search.php
+++ search.php
@@ -5,9 +5,11 @@
 	
 		<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 		
-		<?php get_template_part('loop'); ?>
-		
-		<?php get_template_part('pagination'); ?>
+		<?php while( have_posts() ) : the_post(); ?>
+		<article>
+			<h3><?php the_title(); ?></h3>
+		</article>
+		<?php endwhile; ?>
 	
 	</section>
 	<!-- /section -->
