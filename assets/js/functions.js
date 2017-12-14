36f0b63f8c12d78f7a04805fa1e0f3feffcd327d
regevlio <regevlio@regevlio.com>
2017-12-14 09:43:48 +0200

    Added scripts documentation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,36 +1,3 @@
-function isHome() {
-    if (jQuery('body').hasClass('home')) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isRtl() {
-    if (jQuery('body').hasClass('rtl')) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isMobile(width) {
-    if ( width === null ) {
-        width = 800;
-    }
-
-    if (jQuery(window).width() < width) {
-        return true;
-    } else {
-        return false;
-    }
-}
-function animate_scroll(target){
-    jQuery('html, body').animate({
-        scrollTop: jQuery(target).offset().top
-    }, 1000);
-}
-
 /**************************
     Accessibility
 **************************/
@@ -51,37 +18,58 @@ jQuery('body').on('focusout','ul li.menu-item-has-children.hover ul li:last-chil
 *************************/
 var sliders = [];
 
+/**
+ * Walk through slick slider css selector and generate slick render_sliders
+ *
+ * @return {[type]} [description]
+ */
 function render_sliders(){
 
-    var slider = jQuery(".slick-slider").each( function(){
+    if( typeof slick !='undefined' ){
 
-        var autoplay        = jQuery(this).data( "autoplay" );
-        var dots            = jQuery(this).data( "dots" ) ? jQuery(this).data( "dots" ) : false;
-        var arrows          = jQuery(this).data( "arrows" ) ? jQuery(this).data( "arrows" ) : true;
-        var slidesToShow    = jQuery(this).data( "slidestoshow" ) ? jQuery(this).data( "slidestoshow" ) : 1;
-        var slidesToScroll  = jQuery(this).data( "slidesToScroll" ) ? jQuery(this).data( "slidesToScroll" ) : 1;
+        var slider = jQuery(".slick-slider").each( function(){
 
-        jQuery( this ).slick({
-          slidesToShow: 3,
-          arrows : arrows,
-          slidesToScroll: 1,
-          autoplay: autoplay,
-          autoplaySpeed: 2000,
-          dots : dots
+            var autoplay        = jQuery(this).data( "autoplay" );
+            var dots            = jQuery(this).data( "dots" ) ? jQuery(this).data( "dots" ) : false;
+            var arrows          = jQuery(this).data( "arrows" ) ? jQuery(this).data( "arrows" ) : true;
+            var slidesToShow    = jQuery(this).data( "slidestoshow" ) ? jQuery(this).data( "slidestoshow" ) : 1;
+            var slidesToScroll  = jQuery(this).data( "slidesToScroll" ) ? jQuery(this).data( "slidesToScroll" ) : 1;
+
+            jQuery( this ).slick({
+              slidesToShow: 3,
+              arrows : arrows,
+              slidesToScroll: 1,
+              autoplay: autoplay,
+              autoplaySpeed: 2000,
+              dots : dots
+            });
+
         });
 
-    });
-
-    sliders.push( slider );
+        sliders.push( slider );
+    }
 }
+/*************************
+    GENERAL FUNCTION
+*************************/
 
+/**
+ * Set a cookie
+ * @param {[string]} cname  [holds the name of the cookie]
+ * @param {[string]} cvalue [the cookie value]
+ * @param {[int]} exdays [number of days to hold the cookie]
+ */
 function setCookie( cname, cvalue, exdays ) {
     var d = new Date();
     d.setTime(d.getTime() + ( exdays*24*60*60*1000 ));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-
+/**
+ * Get a saved cookie
+ * @param  {[string]} cname [the nema of the cookie]
+ * @return {[string]}       [returns the saved cookie data]
+ */
 function getCookie( cname ) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -96,4 +84,52 @@ function getCookie( cname ) {
         }
     }
     return "";
+}
+
+/**
+ * A helper function to determine wether this is the home page
+ * @return {Boolean} [description]
+ */
+function isHome() {
+    if (jQuery('body').hasClass('home')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * A helper function to determine wether the user is on a rtl page
+ * @return {Boolean} [description]
+ */
+function isRtl() {
+    if (jQuery('body').hasClass('rtl')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * A helper function to determine wether the user is on a mobile page
+ * @return {Boolean} [description]
+ */
+function isMobile(width) {
+    if ( width === null ) {
+        width = 800;
+    }
+
+    if (jQuery(window).width() < width) {
+        return true;
+    } else {
+        return false;
+    }
+}
+/**
+ * Create scrolled animation
+ * @param  {[string]} target [DOM selector , can be either a css class or ID]
+ *
+ */
+function animate_scroll(target){
+    jQuery('html, body').animate({
+        scrollTop: jQuery(target).offset().top
+    }, 1000);
 }
