3ff55b081e55e60948d32f16d97255026ef3a494
Alex Volkov <vol4ikman@gmail.com>
2018-01-02 10:01:55 +0200

    Update scripts.js

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,20 @@
 jQuery(document).ready(function(){
     
 });
+
+// Detailed list of CF7 events - https://contactform7.com/dom-events/
+
+// Handle contact form 7 "On sent OK" event
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    if ( "4" == event.detail.contactFormId ) {
+        alert( "The contact form ID is 4" );
+        // do something productive
+    }
+}, false );
+// Handle contact form 7 "Submit" event
+document.addEventListener( 'wpcf7submit', function( event ) {
+    if ( '123' == event.detail.contactFormId ) {
+        alert( "The contact form ID is 123." );
+        // do something productive
+    }
+}, false );
