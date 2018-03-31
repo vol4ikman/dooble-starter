22b57a619e74cdaf9f66781fff5b1b1beb120b6b
QuerySolutions <regevlio@querysol.com>
2018-03-31 11:55:43 +0300

    fit fix

---
+++ a/functions/hooks.php
@@ -0,0 +1,40 @@
+<?php
+/*****************************************
+**  Languages
+*****************************************/
+add_action( 'after_setup_theme' , 'qstheme_textdomain' );
+add_filter( 'body_class' , 'add_body_class' );
+
+add_action('acf/init', 'google_api_acf_init');
+add_action('wp_head', 'qs_add_header_scripts');
+add_action('wp_footer', 'qs_add_footer_scripts', 100);
+// Add Theme Stylesheet To ADMIN
+add_action( 'admin_enqueue_scripts', 'qs_admin_theme_styles' );
+// Register THEME Navigation
+add_action('init', 'register_theme_menus');
+
+// Remove Actions
+remove_action( 'wp_head' , 'feed_links_extra' , 3); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head' , 'feed_links' , 2); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head' , 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head' , 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head' , 'index_rel_link' ); // Index link
+remove_action( 'wp_head' , 'parent_post_rel_link' , 10, 0); // Prev link
+remove_action( 'wp_head' , 'start_post_rel_link' , 10, 0); // Start link
+remove_action( 'wp_head' , 'adjacent_posts_rel_link' , 10, 0); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head' , 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+remove_action( 'wp_head' , 'start_post_rel_link' , 10, 0);
+remove_action( 'wp_head' , 'adjacent_posts_rel_link_wp_head' , 10, 0);
+remove_action( 'wp_head' , 'rel_canonical' );
+remove_action( 'wp_head' , 'wp_shortlink_wp_head' , 10, 0);
+// Add Filters
+add_filter( 'widget_text' , 'do_shortcode' ); // Allow shortcodes in Dynamic Sidebar
+add_filter( 'widget_text' , 'shortcode_unautop' ); // Remove <p> tags in Dynamic Sidebars (better!)
+add_filter( 'the_excerpt' , 'shortcode_unautop' ); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
+add_filter( 'the_excerpt' , 'do_shortcode' ); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
+add_filter( 'wp_calculate_image_srcset_meta', '__return_null' ); // Remove thumbnail src set for responsive images
+// Remove Filters
+remove_filter( 'the_excerpt' , 'wpautop' ); // Remove <p> tags from Excerpt altogether
+
+add_action('wp_enqueue_scripts', 'qs_theme_styles');
+add_action( 'wp_enqueue_scripts', 'qs_theme_scripts' );
