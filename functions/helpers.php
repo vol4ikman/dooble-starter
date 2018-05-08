235150d8a72f0a3365132f952c34f5dead7027bf
Alex Volkov <vol4ikman@gmail.com>
2018-05-08 08:07:06 +0300

    allow to editor role access wp menus

--- functions/helpers.php
+++ functions/helpers.php
@@ -108,3 +108,9 @@ function header_menu() {
 		)
 	);
 }
+
+// get the the role object
+$role_object = get_role( 'editor' );
+
+// add $cap capability to this role object
+$role_object->add_cap( 'edit_theme_options' );
