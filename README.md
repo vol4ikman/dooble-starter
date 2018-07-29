28da77817b7cea3aeeacb9326dbeadf4a5e55188
querysol <regevlio@querysol.com>
2018-07-29 11:24:17 +0300

    QS Starter - child theme has been added. Moved from slick to swiper.

--- README.md
+++ README.md
@@ -58,10 +58,15 @@ wp-config.php
 ```
 
 <hr>
-<h3>Important notice!!!</h3>
+<h3>Important notice & Change Log</h3>
+<h5>Please read it =)</h5>
 <ol>
-  <li>When you create .ftpconfig file, your "remote" parameter should looks like this <code>"remote": "/public_html",</code></li>
-  <li>Do NOT upload to git wp-admin & wp-includes folders</li>
+    <li>Child theme has been added to QS Starter Theme</li>
+    <li>Theme has been moved to SWIPER Slider - http://idangero.us/swiper/get-started/</li>
+    <li>Slick slider has been droped from the theme</li>
+    <li><strong>Please, remove .ftpconfig file from the server - very important</strong></li>
+    <li>When you create .ftpconfig file, your "remote" parameter should looks like this <code>"remote": "/public_html",</code></li>
+    <li>Do NOT upload to git wp-admin & wp-includes folders</li>
 </ol>
 
 <hr>
---
+++ qs-starter-child/gulpfile.js
@@ -0,0 +1,125 @@
+// Run this command first:
+// npm install gulp gulp-uglify gulp-rename gulp-clean-css gulp-autoprefixer gulp-concat gulp-rtlcss gulp-notify
+
+var gulp         = require('gulp'),
+    uglify       = require('gulp-uglify'),
+    rename       = require('gulp-rename'),
+    cleanCSS     = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat       = require('gulp-concat'),
+    rtlcss       = require('gulp-rtlcss'),
+    notify       = require("gulp-notify");
+
+/*******************************
+    Define CSS Framework
+*******************************/
+var framework = 'foundation'; // foundation or bootstrap <==================
+
+if( framework == 'foundation' ) {
+    var framework_js = './assets/' + framework + '/js/foundation.min.js';
+    var framework_css = './assets/' + framework + '/css/foundation.min.css';
+} else if( framework == 'bootstrap' ) {
+    var framework_js = './assets/' + framework + '/js/bootstrap.min.js';
+    var framework_css = './assets/' + framework + '/css/bootstrap.min.css';
+}
+
+gulp.task('default', function(){
+    console.log("Gulp default started");
+});
+
+/*************************
+*****  Development *****
+**************************/
+
+var source_scripts = [
+    framework_js,
+    './assets/js/magnific.js',
+    //'./assets/js/select2.js'
+    './assets/js/swiper.min.js',
+    './assets/js/functions.js'
+];
+
+gulp.task('js', function() {
+  return gulp.src(source_scripts)
+    .pipe(concat('assets.js'))                          // create main.js file
+    .pipe(gulp.dest('./build/js/'))                     // move it to build/js/ directory
+    .pipe(rename('assets.min.js'))                      // rename it
+    .pipe(uglify())                                     // minify js
+    .pipe(gulp.dest('./build/js/'))                     // move it again to build/js/ directory
+    .pipe(notify("Scripts compliled + minified"));      // notify message
+});
+
+var source_styles = [
+    './assets/css/normalize.css',
+    framework_css,
+    './assets/css/animate.css',
+    //'./assets/css/select2.css',
+    './assets/css/magnific.css',
+    './assets/css/swiper.min.css'
+];
+
+// Auto generate css to RTL
+gulp.task('css-to-rtl', function () {
+    return gulp.src('./build/css/main-style.css')
+        .pipe(rename('main.style.rtl.css'))
+        .pipe(rtlcss())
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('css', ['js'], function() {
+  return gulp.src(source_styles)
+    .pipe(concat('assets.css'))                        // create style.css file
+    .pipe(gulp.dest('./build/css/'))                   // move it to build/css/ directory
+    .pipe(rename('assets.min.css'))                    // rename it
+    .pipe(cleanCSS())                                  // minify css
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./build/css/'))                    // move it again to build/clean/ directory
+    .pipe(notify("Styles compliled + minified"));       // notify message
+});
+
+gulp.task('dev', ['css'], function(){
+    console.log("Development scripts & styles compiled!!!");
+});
+
+/*************************
+*****  Production  *****
+**************************/
+var production_scripts = [
+    './build/js/assets.min.js',
+    './build/js/scripts.js'
+];
+
+gulp.task('production-js', function() {
+    return gulp.src(production_scripts)
+      .pipe(concat('production.min.js'))              // create main.js file
+      .pipe(gulp.dest('./build/js/'))                // move it to build/js/ directory
+      .pipe(uglify())                               // minify js
+      .pipe(gulp.dest('./build/js/'))               // move it again to build/js/ directory
+      .pipe(notify("Production script done"));      // notify message
+});
+
+var production_styles = [
+    './build/css/assets.min.css',
+    './build/css/main-style.css',
+    './build/css/responsive.css'
+];
+
+gulp.task('production-css',['production-js'], function() {
+    return gulp.src(production_styles)        // move it to build/css/ directory
+    .pipe(rename('production.min.css'))       // rename it
+    .pipe(cleanCSS())                         // minify css
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(concat('production.min.css'))
+    .pipe(gulp.dest('./build/css/'))          // move it again to build/clean/ directory
+    .pipe(notify("Production style done"));   // notify message
+});
+
+gulp.task('production', ['production-css'], function(){
+    console.log("Production executed!!!");
+});
