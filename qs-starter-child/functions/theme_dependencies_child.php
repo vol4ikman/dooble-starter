6af5868abd6eb557d993125c39ea7f3b032e45ff
Alex Volkov <vol4ikman@gmail.com>
2018-10-18 08:01:38 +0300

    Update theme_dependencies_child.php

--- qs-starter-child/functions/theme_dependencies_child.php
+++ qs-starter-child/functions/theme_dependencies_child.php
@@ -1,4 +1,5 @@
 <?php 
+get_template_part('functions/enqueue_child');
 get_template_part( 'functions/hooks_child' );
 get_template_part( 'functions/ajax_child' );
 get_template_part( 'functions/helpers_child' );
