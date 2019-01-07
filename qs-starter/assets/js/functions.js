eb812957ab46be40ddc59337637b299513211537
Alex Volkov <vol4ikman@gmail.com>
2019-01-07 08:27:33 +0200

    inview function has been added

--- qs-starter/assets/js/functions.js
+++ qs-starter/assets/js/functions.js
@@ -138,3 +138,29 @@ function animate_scroll(target){
         scrollTop: jQuery(target).offset().top
     }, 1000);
 }
+
+/**
+* Check if element in view
+* Usage: var isInView = Utils.isElementInView(jQuery('.someselector'), false);
+*
+*/
+function Utils() {}
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = jQuery(window).scrollTop();
+        var pageBottom = pageTop + jQuery(window).height();
+        if( typeof jQuery(element).offset() != 'undefined' ){
+            var elementTop = jQuery(element).offset().top;
+            var elementBottom = elementTop + jQuery(element).height();
+
+            if (fullyInView === true) {
+                return ((pageTop < elementTop) && (pageBottom > elementBottom));
+            } else {
+                return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+            }
+        }
+
+    }
+};
+var Utils = new Utils();
