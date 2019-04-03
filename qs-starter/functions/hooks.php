10bc50e86beb5746ce6f0158662f8979ce9f3cf2
Alex Volkov <vol4ikman@gmail.com>
2019-04-03 07:51:01 +0300

    Update hooks.php

--- qs-starter/functions/hooks.php
+++ qs-starter/functions/hooks.php
@@ -27,6 +27,10 @@ remove_action( 'wp_head' , 'start_post_rel_link' , 10, 0);
 remove_action( 'wp_head' , 'adjacent_posts_rel_link_wp_head' , 10, 0);
 remove_action( 'wp_head' , 'rel_canonical' );
 remove_action( 'wp_head' , 'wp_shortlink_wp_head' , 10, 0);
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); 
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' ); 
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); 
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 // Add Filters
 add_filter( 'widget_text' , 'do_shortcode' ); // Allow shortcodes in Dynamic Sidebar
 add_filter( 'widget_text' , 'shortcode_unautop' ); // Remove <p> tags in Dynamic Sidebars (better!)
