26222cbdce02cfce7aabf26c8b33c68dbcecd593
Alex Volkov <vol4ikman@gmail.com>
2019-05-16 09:15:13 +0300

    Update theme_dependencies_child.php

--- qs-starter-child/functions/theme_dependencies_child.php
+++ qs-starter-child/functions/theme_dependencies_child.php
@@ -4,3 +4,4 @@ get_template_part( 'functions/hooks_child' );
 get_template_part( 'functions/ajax_child' );
 get_template_part( 'functions/helpers_child' );
 get_template_part( 'functions/woocommerce_child' );
+get_template_part( 'admin/functions' );
