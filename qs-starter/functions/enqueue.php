039dbe5ae9fdd136bedeada0bb7605c3181ebe07
Alex Volkov <vol4ikman@gmail.com>
2023-08-12 16:21:44 +0300

    Update enqueue.php

--- qs-starter/functions/enqueue.php
+++ qs-starter/functions/enqueue.php
@@ -51,7 +51,9 @@ function qs_theme_scripts() {
 	wp_enqueue_script( 'scripts' );
 	wp_enqueue_script( 'a11y' );
 
-	wp_register_script( 'ajax_custom_script', THEME . '/assets/js/ajax.js', array( 'scripts' ), THEME_VER, true );
+	wp_register_script( 'ajax_custom_script', THEME . '/assets/js/ajax.js', array( 'scripts' ), THEME_VER, array(
+        'in_footer' => true,
+    ) );
 	wp_localize_script( 'ajax_custom_script', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
 	wp_enqueue_script( 'ajax_custom_script' );
 }
@@ -60,7 +62,9 @@ function qs_theme_scripts() {
  */
 function qs_load_custom_admin_style() {
 	wp_register_style( 'qs-admin-style', get_template_directory_uri() . '/admin/css/admin-style.css', false, '1.0.0' );
-	wp_register_script( 'qs-admin-script', get_template_directory_uri() . '/admin/js/admin-script.js', array( 'jquery' ), THEME_VER, true );
+	wp_register_script( 'qs-admin-script', get_template_directory_uri() . '/admin/js/admin-script.js', array( 'jquery' ), THEME_VER, array(
+        'in_footer' => true,
+    ));
 
 	wp_enqueue_style( 'qs-admin-style' );
 	wp_enqueue_script( 'qs-admin-script' );
